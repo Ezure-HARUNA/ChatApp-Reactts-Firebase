f71d3166de345af4fc2526099ccbba2467f6a4b2
ezure haruna <burimofumofu@gmail.com>
2021-04-23 11:10:53 +0900

    feat:8_IDの表示

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,13 +1,51 @@
-import React from "react";
-import { auth } from "../firebase";
-import ChatInput from "./ChatInput"
-import styles from "./Feed.module.css"
+import React, { useState, useEffect } from "react";
+import styles from "./Feed.module.css";
+import { db } from "../firebase";
+import ChatInput from "./ChatInput";
+// import Post from "./Post";
 
-const Feed = () => {
+const Feed: React.FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      avatar: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        )
+      );
+    return () => {
+      unSub();
+    };
+  }, []);
+
   return (
     <div className={styles.feed}>
-      <ChatInput/>
-      <button onClick={() => auth.signOut()}>Logout</button>
+      <ChatInput />
+
+      
+          {posts.map((post) => (
+            <h2>{post.id}</h2>
+          ))}
+      
     </div>
   );
 };
